d6c71cf5c433ff0a4dda4174b1734b4a0779a147
JorshuaG <joshbgearheart@gmail.com>
2021-09-01 12:15:43 -1000

    finished get, getid, post, putid

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -5,29 +5,42 @@ const {
   validatePost,
 } = require("../middleware/middleware");
 
+const User = require("./users-model");
+const Post = require("../posts/posts-model");
+
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+router.get("/", (req, res, next) => {
+  User.get()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch(next);
 });
 
 router.get("/:id", validateUserId, (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
+  res.json(req.user);
 });
 
-router.post("/", validateUser, (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+router.post("/", validateUser, (req, res, next) => {
+  User.insert({ name: req.name })
+    .then((newUser) => {
+      res.status(201).json(newUser);
+    })
+    .catch(next);
 });
 
-router.put("/:id", validateUserId, (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+router.put("/:id", validateUserId, validateUser, (req, res, next) => {
+  User.update(req.params.id, { name: req.name })
+    .then(() => {
+      return User.getById(req.params.id).then((user) => {
+        res.json(user);
+      });
+    })
+    .catch(next);
 });
 
 router.delete("/:id", validateUserId, (req, res) => {
@@ -52,5 +65,12 @@ router.post(
   }
 );
 
-// do not forget to export the router
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    customMessage: "Don't panic, did you try using 42?",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
 module.exports = router;
