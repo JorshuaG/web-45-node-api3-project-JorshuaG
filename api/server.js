605b4f1c862c9dc7505db15fb8c205de1b4b0ef1
JorshuaG <joshbgearheart@gmail.com>
2021-09-01 10:45:23 -1000

    logger middleware

--- api/server.js
+++ api/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
+const { logger } = require("./middleware/middleware");
 const usersRouter = require("./users/users-router");
 const server = express();
 server.use(express.json());
-// remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
 server.use("/api/users", usersRouter);
