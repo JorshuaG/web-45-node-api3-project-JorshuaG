fbae340777fc749f55767592ce8a59a7c22587e3
JorshuaG <joshbgearheart@gmail.com>
2021-09-01 10:58:07 -1000

    validateuserId middlware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,6 @@
+const User = require("../users/users-model");
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
   const timestamp = -new Date().toLocaleString();
   const method = req.method;
   const url = req.original;
@@ -7,10 +8,18 @@ function logger(req, res, next) {
   next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
-  console.log("validateUserId middleware");
-  next();
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id);
+    if (!user) {
+      res.status(404).json({ message: "User does not not exist" });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Problem finding user" });
+  }
 }
 
 function validateUser(req, res, next) {
